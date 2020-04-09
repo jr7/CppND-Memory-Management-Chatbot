f5adf5206b18be630a85439dfaae9c7fa8ed8333
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2020-04-09 15:08:53 +0200

    Add chatbot with rule of five

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -30,6 +30,69 @@ ChatBot::ChatBot(std::string filename)
     _image = new wxBitmap(filename, wxBITMAP_TYPE_PNG);
 }
 
+//// STUDENT CODE
+////
+
+ //copy constructor
+ChatBot::ChatBot(const ChatBot &source){
+    std::cout << "ChatBot CopyConstructor" << std::endl;
+    _image = new wxBitmap(*source._image);
+
+    _chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+    _currentNode = source._currentNode;
+}
+
+//assignment operator
+ChatBot& ChatBot::operator=(const ChatBot &source){
+    std::cout << "ChatBot Assignment Operator" << std::endl;
+    if (this == &source)
+        return *this;
+
+    delete[] _image;
+    _image = new wxBitmap(*source._image);
+    _chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+    _currentNode = source._currentNode;
+    return *this;
+}
+//move constructor
+ChatBot::ChatBot(ChatBot &&source){
+    std::cout << "ChatBot Move Constructor" << std::endl;
+    _image = source._image;
+    _chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+    _currentNode = source._currentNode;
+
+    source._image = nullptr;
+    source._chatLogic = nullptr;
+    source._rootNode = nullptr;
+    source._currentNode = nullptr;
+}
+
+//move assignment operator
+ChatBot& ChatBot::operator=(ChatBot &&source){
+    std::cout << "ChatBot Move Assignment" << std::endl;
+    if (this == &source)
+        return *this;
+
+    _image = source._image;
+    _chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+    _currentNode = source._currentNode;
+
+    source._image = nullptr;
+    source._chatLogic = nullptr;
+    source._rootNode = nullptr;
+    source._currentNode = nullptr;
+
+    return *this;
+}
+
+////
+//// EOF STUDENT CODE
+
+//destructor
 ChatBot::~ChatBot()
 {
     std::cout << "ChatBot Destructor" << std::endl;
@@ -42,11 +105,6 @@ ChatBot::~ChatBot()
     }
 }
 
-//// STUDENT CODE
-////
-
-////
-//// EOF STUDENT CODE
 
 void ChatBot::ReceiveMessageFromUser(std::string message)
 {
