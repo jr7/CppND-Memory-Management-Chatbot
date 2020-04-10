500bac98e85b9650859705f6811b494eaaae6405
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2020-04-10 15:26:29 +0200

    Add task 4

--- src/graphnode.h
+++ src/graphnode.h
@@ -3,7 +3,11 @@
 
 #include <vector>
 #include <string>
+#include <memory>
+
+
 #include "chatbot.h"
+
 
 
 // forward declarations
@@ -16,7 +20,7 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
@@ -44,7 +48,7 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
